6ec8df0b43f69ca083574c7625fee22414401e6f
hannan1122-bit <145362880+hannan1122-bit@users.noreply.github.com>
2024-02-22 07:32:57 +0500

    i have changed nothing

---
+++ src/app/Top.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Top = () => {
+  return (
+    <div className="Top">
+      <div className="h-20 px-10 w-1/2 flex items-center justify-center">
+    <h2 className="font-bold text-3xl">Kankai Rubber Company</h2>
+    </div>
+    <p>welcome to Kankai Rubber Company how we can help you in this</p>
+    </div>
+  )
+}
+
+export default Top
